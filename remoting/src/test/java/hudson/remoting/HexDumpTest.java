9c17362024d4cb82df2171dc3fd93f34e30f4a46
Brian Atkinson <nairb774@gmail.com>
2011-02-06 19:09:48 -0600

    Move HexDumpTest to JUnit4.

--- remoting/src/test/java/hudson/remoting/HexDumpTest.java
+++ remoting/src/test/java/hudson/remoting/HexDumpTest.java
@@ -1,11 +1,14 @@
 package hudson.remoting;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 
 /**
  * @author Kohsuke Kawaguchi
  */
-public class HexDumpTest extends TestCase {
+public class HexDumpTest {
+    @Test
     public  void test1() {
         assertEquals("0001ff",HexDump.toHex(new byte[]{0,1,-1}));
     }
